d8b734abe1d3414b98817d0424b76cb425b55393
Matthias Huber <66099549+MatthiasHuber-Digital@users.noreply.github.com>
2023-01-06 07:10:33 +0300

    system recognizes and denies blocked moves
    This change affects rooks, queens, bishops.

--- com/chess/piece/Bishop.java
+++ com/chess/piece/Bishop.java
@@ -1,5 +1,6 @@
 package com.chess.piece;
 import java.util.*;
+import java.util.stream.Collectors;
 import com.chess.squares.*;
 import com.chess.board.*;
 import com.chess.common.*;
@@ -10,7 +11,7 @@ public class Bishop extends AbstractPiece{
         super(pieceColor);
         this.name = "Bishop";
     }
-
+    /* 
     @Override
     public List<Location> getValidMoves(Board board){
         Map<Location, Square> squareMap = board.getLocationSquareMap();
@@ -56,11 +57,45 @@ public class Bishop extends AbstractPiece{
             next = LocationFactory.buildLocation(next, fileOffset, rankOffset);
         }
     }
+    */
 
-/*     @Override
-    public void makeMove(Square square){
-        Square current = this.getCurrentSquare();
-        this.setCurrentSquare(square);
-        current.reset();
-    }; */
+    @Override
+    public List<Location> getValidMoves(Board board){
+
+        Map<Location, Square> squareMap = board.getLocationSquareMap();
+        Location currentLocation = this.getCurrentSquare().getLocation();
+        ArrayList<Location> moveCandidates = new ArrayList<>();
+        ArrayList<Location> newMoveCandidates = new ArrayList<>();
+
+        // FILE (A), then RANK(1)
+        int[][] offsets = {
+            {1, 1}, 
+            {-1, -1}, 
+            {-1, 1}, 
+            {1, -1},
+        };
+
+        /* 
+         *         int[][] tempOffsets = new int[4][2];
+
+        int moveMultiplier = 1;
+        newMoveCandidates = this.filterMovesInBoard(offsets, currentLocation);
+        while (!newMoveCandidates.isEmpty()){
+            for (Location candidate : newMoveCandidates){
+                moveCandidates.add(candidate);
+            }
+            moveMultiplier++;
+            for (int i=0; i<offsets.length; i++) {
+                tempOffsets[i][0] = offsets[i][0] * moveMultiplier;
+                tempOffsets[i][1] = offsets[i][1] * moveMultiplier;
+              }
+            newMoveCandidates = this.filterMovesInBoard(tempOffsets, currentLocation);
+        }
+         * 
+        */
+
+        moveCandidates = this.filterStraightMovesInBoard(offsets, currentLocation);
+
+        return this.filterUnblockedStraightMoves(moveCandidates, squareMap, currentLocation, offsets);
+    }
 }
--- com/chess/piece/Rook.java
+++ com/chess/piece/Rook.java
@@ -10,57 +10,43 @@ public class Rook extends AbstractPiece{
         super(pieceColor);
         this.name = "Rook";
     }
-    
+
     @Override
-    public ArrayList<Location> getValidMoves(Board board){
+    public List<Location> getValidMoves(Board board){
+
         Map<Location, Square> squareMap = board.getLocationSquareMap();
-        return getSquareDependentMoveCandidates(this.getCurrentSquare(), squareMap);
-    }
-
-    private ArrayList<Location> getSquareDependentMoveCandidates(
-        Square square,
-        Map<Location, Square> squareMap
-        ){
-        Location currentLocation = square.getLocation();
+        Location currentLocation = this.getCurrentSquare().getLocation();
         ArrayList<Location> moveCandidates = new ArrayList<>();
-        getMoves(moveCandidates, squareMap, currentLocation, 1, 0);
-        getMoves(moveCandidates, squareMap, currentLocation, -1, 0);
-        getMoves(moveCandidates, squareMap, currentLocation, 0, 1);
-        getMoves(moveCandidates, squareMap, currentLocation, 0, -1);
-        return moveCandidates;
-    }
 
-    private void getMoves(
-        ArrayList<Location> candidates,
-        Map<Location, Square> squareMap,
-        Location currentLocation,
-        int rankOffset,
-        int fileOffset
-    ){
-        Location next = LocationFactory.buildLocation(currentLocation, fileOffset, rankOffset);
-        while(squareMap.containsKey(next)){
-            // if the next square is occupied
-            if (squareMap.get(next).getIsOccupied()){
-                // in case of SAME color - don't add the square (obstacle)
-                if (squareMap.get(next).getCurrentPiece().pieceColor.equals(this.pieceColor)) 
-                    break;
-                else{
-                    // in case of OTHER color - add square
-                    candidates.add(next);
-                    // ...and do NOT UPDATE to next potential location!
-                    break;
+        // FILE (A), then RANK(1)
+        int[][] offsets = {
+            {1, 0}, 
+            {-1, 0}, 
+            {0, 1}, 
+            {0, -1},
+        };
+
+        /* 
+                int[][] tempOffsets= new int[4][2];
+
+            int moveMultiplier = 1;
+            ArrayList<Location> newMoveCandidates = this.filterMovesInBoard(offsets, currentLocation);
+            while (!newMoveCandidates.isEmpty()){
+                for (Location candidate : newMoveCandidates){
+                    moveCandidates.add(candidate);
                 }
+                moveMultiplier++;
+                for (int i=0; i<offsets.length; i++) {
+                    tempOffsets[i][0] = offsets[i][0] * moveMultiplier;
+                    tempOffsets[i][1] = offsets[i][1] * moveMultiplier;
+                }
+                newMoveCandidates = this.filterMovesInBoard(tempOffsets, currentLocation);
             }
-            candidates.add(next); // this candidate is OK
-            // update the next location based on the offsets:
-            next = LocationFactory.buildLocation(next, fileOffset, rankOffset);
-        }
-    }
+        */
 
-/*     @Override
-    public void makeMove(Square square){
-        Square current = this.getCurrentSquare();
-        this.setCurrentSquare(square);
-        current.reset();
-    }; */
+        moveCandidates = this.filterStraightMovesInBoard(offsets, currentLocation);
+
+        return this.filterUnblockedStraightMoves(moveCandidates, squareMap, currentLocation, offsets);
+
+    }
 }
